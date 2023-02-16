291855498f0cb837b875d6c385b5bda6fed6abb2
Andres Amaro <lionAS_45@hotmail.com>
2023-02-16 01:17:47 -0500

    Actualizacion de website

---
+++ styles/theme.js
@@ -0,0 +1,31 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  styles: {
+    global: (props) => ({
+      body: {
+        margin: 0,
+        padding: 0,
+        minHeight: '100vh',
+        color: 'white',
+        background: '#ddd',
+        '--gradient': 'linear-gradient(45deg, #845ec2, #d65db1, #ff5f91, #ff9671, #ffc75f, #f9f871)',
+        backgroundImage: 'var(--gradient)',
+        // backgroundSize: '400%',
+        animation: 'bg-animation 20s infinite alternate',
+      },
+    }),
+  },
+  animations: {
+    '@keyframes bg-animation': {
+      '0%': {
+        'background-position': 'left',
+      },
+      '100%': {
+        'background-position': 'right',
+      },
+    },
+  },
+});
+
+export default theme;
