103e5724a360fc73e440bf856e2e8d02514be00f
Andres Amaro <lionAS_45@hotmail.com>
2023-02-17 05:16:30 -0500

    comentario

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '@/styles/theme';
 export default function App({ Component, pageProps }) {
+  //Extendiendo el tema
   return <ChakraProvider theme={theme}>
          <Component {...pageProps} />
          </ChakraProvider>
